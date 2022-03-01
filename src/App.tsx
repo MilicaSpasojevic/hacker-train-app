f6403c3cbdbeb24050dbd5d1f64a7c5fc4b22cd4
infin1t3 <jovanovicv90@gmail.com>
2022-03-01 12:58:24 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import { WalletWrapper } from './components/Wallet/WalletWrapper';
+import {WalletWrapper} from './components/Wallet/WalletWrapper';
 import Navbar from './components/Navbar/Navbar';
 import NftCard from './components/NftCard/NftCard';
 
+const nfts = [1,2,3,4,5,6]
+
 function App() {
+
+  const [selectedNfts, toggleNft] = useState([]);
+
   return (
     <WalletWrapper>
       <div className="App">
         <Navbar/>
-        <NftCard/>
+        <div className="App_nft_card">
+          {nfts.map((item, index) => {
+            return <NftCard toggleNft={toggleNft} id={index}  />
+          })}
+        </div>
       </div>
     </WalletWrapper>
 
--- src/components/NftCard/NftCard.tsx
+++ src/components/NftCard/NftCard.tsx
@@ -7,9 +7,9 @@ interface NftCardProps {
   nft: Nft;
 }
 
-const NftCard: React.FC<any> = () => {
+const NftCard: React.FC<any> = ({ toggleNft, id}) => {
   return (
-    <div className="nft-card">
+    <div className="nft-card" onClick={()=> toggleNft(id)}>
       <img src="https://unq-test.s3.eu-central-1.amazonaws.com/p3.jpeg" alt="Name" />
       <h3>Monkey</h3>
     </div>
