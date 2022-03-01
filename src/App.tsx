76e560a02b209a4274bcdc2bc5467ce0e5bec154
infin1t3 <jovanovicv90@gmail.com>
2022-03-01 12:43:40 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import { WalletWrapper } from './components/Wallet/WalletWrapper';
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
 import Navbar from './components/Navbar/Navbar';
 import NftCard from './components/NftCard/NftCard';
 
